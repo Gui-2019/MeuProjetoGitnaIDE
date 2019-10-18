98781716fad13a987ccec6e20f3a07c4730efc56
Guilherme Graeff <guilhermegraeff95@gmail.com>
2019-10-18 19:48:15 -0300

    Nova linha adicionada na classe principal

--- src/meuprojetogitnaide/MeuProjetoGitnaIDE.java
+++ src/meuprojetogitnaide/MeuProjetoGitnaIDE.java
@@ -15,6 +15,7 @@ public class MeuProjetoGitnaIDE {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Seja bem vindo ao GIT no NetBeans");
        
     }
     
